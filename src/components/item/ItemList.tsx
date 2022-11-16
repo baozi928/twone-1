7c4421c7e25f72030ad0e618f364adfb3a920765
baozi928 <2542101862@qq.com>
2022-11-16 15:43:55 +0800

    修复时间bug

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -30,15 +30,16 @@ export const ItemList = defineComponent({
         end: time.lastDayOfYear()
       }
     ]
-    watchEffect(() => {
-        if (refSelected.value === '自定义时间') {
-          refOverlayVisible.value = true
-        }
-      })
+
       const refOverlayVisible = ref(false)
       const onSubmitCustomTime = (e: Event) => {
         e.preventDefault()
         refOverlayVisible.value = false
+      }
+      const onSelect = (value: string) => {
+        if (value === '自定义时间') {
+          refOverlayVisible.value = true
+        }
       }
     return () => (
           <MainLayout>{
@@ -46,7 +47,8 @@ export const ItemList = defineComponent({
           title: () => '柿子记账',
           icon: () => <Icon name="menu" />,
           default: () => <>
-            <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+            <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}
+              onUpdate:selected={onSelect}>
               <Tab name="本月">
               <ItemSummary
                   startDate={timeList[0].start.format()}
@@ -79,7 +81,7 @@ export const ItemList = defineComponent({
                     <FormItem label='结束时间' v-model={customTime.end} type='date' />
                     <FormItem>
                       <div class={s.actions}>
-                        <button type="button">取消</button>
+                        <button type="button" onClick={() => refOverlayVisible.value = false}>取消</button>
                         <button type="submit">确认</button>
                       </div>
                     </FormItem>
