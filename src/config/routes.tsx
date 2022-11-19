2f724b02cf60082bcb087e69af59b96920ce08c2
baozi928 <2542101862@qq.com>
2022-11-19 12:51:41 +0800

    没有登录就不能记账

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -17,6 +17,7 @@ import { TagCreate } from "../components/tag/TagCreate";
 import { TagEdit } from "../components/tag/TagEdit";
 import { TagPage } from "../views/TagPage";
 import { StatisticsPage } from "../views/StatisticsPage";
+import { http } from "../shared/Http";
 
 export const routes: RouteRecordRaw[] = [
   { path: "/", redirect: "/welcome" },
@@ -37,6 +38,12 @@ export const routes: RouteRecordRaw[] = [
   {path:'/start', component: StartPage},
   {
     path:'/items',component:ItemPage,
+    beforeEnter: async (to, from, next) => {
+      await http.get('/me').catch(() => {
+        next('/sign_in?return_to=' + to.path)
+      })
+      next()
+    },
     children:[
       {path:'',component:ItemList},
       {path:'create',component:ItemCreate}
