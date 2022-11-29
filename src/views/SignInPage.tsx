144192d9cdf704a28292f01d54ed18db80baa0be
baozi928 <2542101862@qq.com>
2022-11-30 02:15:44 +0800

    取消显示今年

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -39,7 +39,6 @@ export const SignInPage = defineComponent({
       if (!hasError(errors)) {
         const response = await http.post<{ jwt: string }>('/session', formData)
         .catch(onError)
-        console.log(response)
         localStorage.setItem('jwt', response.data.jwt)
         const returnTo = route.query.return_to?.toString()
         refreshMe()
