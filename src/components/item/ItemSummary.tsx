9c99e27f2ec086a57f233b954442bf2aa9154131
baozi928 <2542101862@qq.com>
2022-11-30 00:36:14 +0800

    items 可以按照自定义时间查询

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, onMounted, PropType, reactive,ref } from 'vue'
+import { defineComponent, onMounted, PropType, reactive,ref , watch} from 'vue'
 import { Button } from '../../shared/Button'
 import { FloatButton } from '../../shared/FloatButton'
 import { Datetime } from '../../shared/Datetime'
@@ -34,10 +34,16 @@ export const ItemSummary = defineComponent({
       page.value += 1
     }
     onMounted(fetchItems)
+    watch(()=>[props.startDate,props.endDate], ()=>{
+      items.value = []
+      hasMore.value = false
+      page.value = 0
+      fetchItems()
+    })
     const itemsBalance = reactive({
       expenses: 0, income: 0, balance: 0
     })
-    onMounted(async ()=>{
+    const fetchItemsBalance =async ()=>{
       if(!props.startDate || !props.endDate){ return }
       const response = await http.get('/items/balance', {
         happen_after: props.startDate,
@@ -46,6 +52,13 @@ export const ItemSummary = defineComponent({
         _mock: 'itemIndexBalance',
       })
       Object.assign(itemsBalance, response.data)
+    }
+    onMounted(fetchItemsBalance)
+    watch(()=>[props.startDate,props.endDate], ()=>{
+      Object.assign(itemsBalance, {
+        expenses: 0, income: 0, balance: 0
+      })
+      fetchItemsBalance()
     })
     return () => (
       <div class={s.wrapper}>
@@ -69,11 +82,11 @@ export const ItemSummary = defineComponent({
               {items.value.map((item) => (
                 <li>
                   <div class={s.sign}>
-                    <span>{item.tags_id[0]}</span>
+                  <span>{item.tags![0].sign}</span>
                   </div>
                   <div class={s.text}>
                     <div class={s.tagAndAmount}>
-                      <span class={s.tag}>{item.tags_id[0]}</span>
+                    <span class={s.tag}>{item.tags![0].name}</span>
                       <span class={s.amount}>￥<Money value={item.amount}/></span>
                     </div>
                     <div class={s.time}><Datetime value={item.happen_at}/></div>
