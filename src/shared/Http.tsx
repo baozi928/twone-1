fdf10d8ef689f086dc39fa07d6fd8d30c9f78e08
baozi928 <2542101862@qq.com>
2022-11-30 00:18:42 +0800

    获取 items 支出、收入、余额统计

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,5 +1,5 @@
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-import { mockTagEdit,mockItemIndex,mockSession,mockItemCreate,mockTagIndex, mockTagShow } from "../mock/mock";
+import { mockTagEdit,mockItemIndexBalance, mockItemIndex,mockSession,mockItemCreate,mockTagIndex, mockTagShow } from "../mock/mock";
 
 type GetConfig = Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>
 type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
@@ -55,6 +55,9 @@ const mock = (response: AxiosResponse) => {
       return true
     case 'itemIndex':
       [response.status, response.data] = mockItemIndex(response.config)
+      return true
+    case 'itemIndexBalance':
+      [response.status, response.data] = mockItemIndexBalance(response.config)
       return true
   }
   return false
