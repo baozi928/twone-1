db09819bcf4c3f8f95cb183f070e9fe40756a1ad
baozi928 <2542101862@qq.com>
2022-12-03 01:05:08 +0800

    修复错误地请求下一页

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, onMounted, PropType, reactive,ref , watch} from 'vue'
+import { defineComponent, onUnmounted,onMounted, PropType, reactive,ref , watch} from 'vue'
 import { useAfterMe } from '../../hooks/useAfterMe'
 import { Button } from '../../shared/Button'
 import { FloatButton } from '../../shared/FloatButton'
@@ -107,7 +107,7 @@ export const ItemSummary = defineComponent({
             </ol>
             <div class={s.more}>
             {itemStore.hasMore ? (
-                <Button onClick={() => itemStore.fetchItems(props.startDate, props.endDate)}>加载更多</Button>
+                <Button onClick={() => itemStore.fetchNextPage(props.startDate, props.endDate)}>加载更多</Button>
               ) : (
                 <span>没有更多</span>
                 )}
